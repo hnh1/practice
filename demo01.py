66999cb5f986e176d7544de16cb3c0a5498cf7e2
hnh <1024712423@qq.com>
2020-06-19 18:45:05 +0800

    第一次向github提交代码

---
+++ demo01.py
@@ -0,0 +1,52 @@
+'''
+print("hello world!")  #字符串
+print(2333)  #整数
+print(2.3)  #小数
+print(True)  #布尔值:True,False
+print(False)
+print(())  #元组
+print([])  #数组
+print({})  #字典  
+"""
+锄禾日当午
+汗滴禾下土
+"""
+print("hahaha",2333,2.333)
+print("haha"+"xixi")  #字符串的拼接（只能同样的类型进行拼接操作）
+print("hahaha"*100)  #打印100个
+print(((1+2)*100-9.9)/2)
+print((1+1)==2)
+print(2>3)
+#变量，赋值
+a="zhangsan" #把zhangsan这个值赋值给了a这个变量
+print(a)
+'''
+
+# a=input("请输入a：")
+# b=input("请输入b：")
+# print("input获取的内容：",a+b)
+
+#数据格式的转换
+# print(type(2333))
+# print(type("haha"))
+# print(type(2.333))
+# print(type(True))
+# print(type(()))
+# print(type([]))
+# print(type({}))
+
+# a=str(2333)
+# print(type(a))
+# print(type(None))
+# a=int("hahaha")
+# print(type(a))
+# print(int(2.33))
+
+# a=float(input("请输入a:"))
+# b=float(input("请输入b:"))
+# print("获取input的内容：",a+b)
+# print(str(None))
+# print(len('aaaa'))
+# a=input("输入a：")
+# b=input("输入b：")
+# print("a和b的长度和：",len(a)+len(b))
